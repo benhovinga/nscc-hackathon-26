46e87c3b0177a85fa3e0c4ef14f0245cc731b627
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-01-10 18:22:21 -0400

    Prefill some more example endpoints

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,38 @@
-from fastapi import FastAPI
+from enum import Enum
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+
+class Room(BaseModel):
+    class Type(str, Enum):
+        classroom_lab = "classroom_lab"
+        classroom_lecture = "classroom_lecture"
+        study_room = "study_room"
+
+    id: int
+    room_number: str
+    building_wing: str
+    building_floor: int
+    room_type: Type
+    
+
+fake_rooms_db: list[Room] = [
+    Room(
+        id=1,
+        room_number="D225",
+        building_wing="D",
+        building_floor=2,
+        room_type=Room.Type.classroom_lab
+    ),
+    Room(
+        id=2,
+        room_number="D227",
+        building_wing="D",
+        building_floor=2,
+        room_type=Room.Type.classroom_lab
+    ),
+]
 
 
 app = FastAPI()
@@ -23,21 +56,15 @@ app.add_middleware(
 async def read_root() -> dict:
     return {"message": "Welcome to the API."}
 
-@app.get("/room", tags=["room"])
-async def list_rooms() -> list:
-    return [
-        {
-            "_id": 1,
-            "number": 225,
-            "wing": "D",
-            "floor": "2",
-            "type": "classroom:lab"
-        },
-        {
-            "_id": 2,
-            "number": 227,
-            "wing": "D",
-            "floor": "2",
-            "type": "classroom:lab"
-        }
-    ]
+
+@app.get("/room", tags=["room"], response_model=list[Room])
+async def list_rooms() -> list[Room]:
+    return fake_rooms_db
+
+
+@app.get("/room/{room_id}", tags=["room"], response_model=Room)
+async def get_room(room_id: int) -> Room:
+    for room in fake_rooms_db:
+        if room.id == room_id:
+            return room
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
