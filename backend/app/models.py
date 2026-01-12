b00c85b71734308c85752c88016a6445304fe50f
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-01-12 11:05:06 -0400

    Setup more models and sample data (#10)
    * fix merge
    
    * create sample data and models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,16 @@
 # Python built-in library imports
+import datetime
 from enum import Enum
 
 # Third-party library imports (pip)
 from sqlmodel import Field, SQLModel
+
+
+class Season(str, Enum):
+    spring = "spring"
+    summer = "summer"
+    fall = "fall"
+    winter = "winter"
 
 
 class RoomType(str, Enum):
@@ -11,9 +19,52 @@ class RoomType(str, Enum):
     study_room = "study_room"
 
 
+class DaysNoClass(SQLModel, table=True):
+    date: datetime.date = Field(primary_key=True)
+
+
+class DaysNoSchool(SQLModel, table=True):
+    date: datetime.date = Field(primary_key=True)
+
+
+class SchoolTerm(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    season: Season
+    startDate: datetime.date
+    endDate: datetime.date
+
+
 class Room(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    room_number: str
+    room_number: str = Field(index=True)
     building_wing: str
     building_floor: int
     room_type: RoomType
+
+
+class Course(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    code: str = Field(index=True)
+    name: str
+    instructor: str
+    school_term: int = Field(foreign_key="schoolterm.id")
+
+
+class CourseSchedule(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    day_of_Week: int
+    start_time: datetime.time
+    end_time: datetime.time
+    course: int = Field(foreign_key="course.id")
+    room: int = Field(foreign_key="room.id")
+
+
+class RoomBooking(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str
+    start_date_time: datetime.datetime
+    end_date_time: datetime.datetime
+    is_private: bool
+    booked_by: str
+    school_term: int = Field(foreign_key="schoolterm.id")
+    room: int = Field(foreign_key="room.id")
