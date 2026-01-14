68ad58325d414035a75ae34ce6026e68baa63eb8
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-01-14 08:32:25 -0400

    Remove extra filter condition

--- backend/app/api.py
+++ backend/app/api.py
@@ -36,6 +36,7 @@ app.add_middleware(
 def read_root() -> dict:
     return {"message": "Welcome to the API."}
 
+
 @app.get("/rooms", response_model=list[Room])
 def list_rooms(
     room_type: str | None = None,
@@ -86,25 +87,19 @@ def get_room_schedule(room_number: str):
 
 
 @app.get("/courses", response_model=list[Course])
-def list_courses(
-    instructor: str | None = None, 
-    school_term: int | None = None 
-    ):
+def list_courses(instructor: str | None = None, school_term: int | None = None):
     filters = []
-    #collect courses and put into list
-    if instructor and school_term in Course:
-        filters.append(Course.instructor == instructor) #Creates a conditional argument to be used on the where excecution later on
     if instructor:
         filters.append(Course.instructor == instructor)
     if school_term:
         filters.append(Course.school_term == school_term)
 
-    #Find courses using the list of conditionals with "where"
+    # Find courses using the list of conditionals with "where"
     with Session(engine) as session:
         if len(filters) > 0:
             courses = session.exec(select(Course).where(*filters)).all()
         else:
-            #Run if no filters were appended, returns all courses
+            # Run if no filters were appended, returns all courses
             courses = session.exec(select(Course)).all()
         return courses
 
